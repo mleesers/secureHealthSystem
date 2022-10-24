48340e6ab95e83d5384c59aa04333ce7276c7650
Michael Smith <mduron432@gmail.com>
2022-10-24 12:21:25 -0500

    added basic html and css for header to all files

--- loginPageResources/loginPage.js
+++ loginPageResources/loginPage.js
@@ -2,10 +2,11 @@ document.getElementById("login-btn").onclick = function(event){
     
     var username = document.getElementById("username-login").value;
     var password = document.getElementById("password-login").value;
-    if(username == 'mleesers' & password == "88723939Md"){
-        console.log("SUCCESS!");
-    }else{
-        console.log(":(");
-    }
     event.preventDefault();
+    if(username == 'mleesers' & password == "88723939Md"){
+       window.location.href= "file:///C:/Users/mduro/Desktop/secureHealthSystem/mainPageResources/mainPage.html";
+    }else{
+        document.getElementById("wrong").style.display = 'block';
+    }
+    
 };
